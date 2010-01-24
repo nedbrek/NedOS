271f38a41f8ba5c8ad403f209b1364323b4cf4e7
Edward Brekelbaum <nedbrek@yahoo.com>
2010-01-24 12:39:00 +0000

    Added dump for boot record Added skip for first 16 sectors

--- mkcd/cddump.cpp
+++ mkcd/cddump.cpp
@@ -1,24 +1,37 @@
 #include "iso9660.h"
+#include "eltorito.h"
 #include <cstdio>
 
 int main(int argc, char **argv)
 {
+	printf("Sizeof(boot record)=%d\n", sizeof(BootRecord));
+
 	uint8_t *buf = new uint8_t[2048];
 	PrimaryVolDesc *pvd = (PrimaryVolDesc*)buf;
+	BootRecord     *br  =     (BootRecord*)buf;
 
 	printf("Sizeof(pvd)=%d\n", sizeof(PrimaryVolDesc));
 
+	unsigned sectorCt = 0;
+	unsigned bootCatalog = 0;
 	FILE *f = fopen(argv[1], "rb");
-	fread(buf, 2048, 1, f);
-	while( !feof(f) )
+	for(
+	    fread(buf, 2048, 1, f);
+	    !feof(f);
+	    fread(buf, 2048, 1, f), ++sectorCt)
 	{
-		printf("Sector Type: %d\n", pvd->type_);
+		if( sectorCt < 16 ) continue;
+
+		printf("Sector(%3d) Type: %d\n", sectorCt, pvd->type_);
 		if( pvd->type_ == 1 )
 			pvd->print();
+		else if( br->type_ == 0 )
+		{
+			br->print();
+			bootCatalog = br->bootCatLoc_;
+		}
 
 		if( pvd->type_ == 255 ) break; // terminator
-
-		fread(buf, 2048, 1, f);
 	}
 
 	return 0;
