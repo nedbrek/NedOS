cccb8c2fac8ef25601070aec0537cca733d67b48
Edward Brekelbaum <nedbrek@yahoo.com>
2010-01-24 12:01:19 +0000

    Data structures for CDROM

---
+++ mkcd/iso9660.cpp
@@ -0,0 +1,186 @@
+#include "iso9660.h"
+#include <cstring>
+#include <arpa/inet.h>
+#include <cstdio>
+#include <ctype.h>
+
+//----------------------------------------------------------------------------
+bool isPrintable(uint8_t c)
+{
+	if( isalpha(c) ) return true;
+
+	if( '0' <= c && c <= '9' ) return true;
+
+	switch( c )
+	{
+	case '.': return true;
+	case '_': return true;
+	case '(': return true;
+	case ')': return true;
+	}
+	return false;
+}
+
+void printC(uint8_t c)
+{
+	if( isPrintable(c) )
+		printf("%2c ", c);
+	else
+		printf("%02x ", c);
+}
+
+void printStr(uint8_t *cary, unsigned len)
+{
+	if( len < 78 )
+	{
+		printf("'");
+		for(unsigned i = 0; i < len; ++i)
+			printC(cary[i]);
+		printf("'\n");
+		return;
+	}
+	//else
+
+	for(unsigned i = 0; i < len; ++i)
+	{
+		printC(cary[i]);
+		if( len == 16 ) printf("\n");
+	}
+	printf("\n");
+}
+
+//----------------------------------------------------------------------------
+VolDescBase::VolDescBase(void)
+{
+	type_ = 254; // invalid
+	memcpy(cd001_, "CD001", 5);
+	version_ = 1;
+}
+
+//----------------------------------------------------------------------------
+void BiEnd32::set(uint32_t val)
+{
+	little_ = val;
+	big_    = htonl(val);
+}
+
+//----------------------------------------------------------------------------
+void BiEnd16::set(uint16_t val)
+{
+	little_ = val;
+	big_    = htons(val);
+}
+
+//----------------------------------------------------------------------------
+DirEnt::DirEnt(void)
+{
+	memset(weird_, 0, 34);
+}
+
+void DirEnt::print(void)
+{
+	printf("DirEnt in C\n");
+	for(unsigned i = 0; i < 34; ++i)
+	{
+		printC(weird_[i]);
+		if( i == 16 ) printf("\n");
+	}
+	printf("\n");
+
+	printf("DirEnt in 0x\n");
+	for(unsigned i = 0; i < 34; ++i)
+	{
+		printf("%02x ", weird_[i]);
+		if( i == 16 ) printf("\n");
+	}
+	printf("\n");
+}
+
+//----------------------------------------------------------------------------
+DateTime::DateTime(void)
+{
+}
+
+//----------------------------------------------------------------------------
+PrimaryVolDesc::PrimaryVolDesc(void)
+{
+	type_    = 1;
+	unused0_ = 0;
+
+	memset(systemId_, ' ', 32);
+	memcpy(systemId_, "NedOS", 5);
+
+	memset(volumeId_, ' ', 32);
+	static const char *const nedOSvolLabel = "NedOS Boot Disk 0.0";
+	memcpy(volumeId_, nedOSvolLabel, strlen(nedOSvolLabel)-1);
+
+	memset(unused1_, 0, 8);
+	memset(unused2_, 0, 32);
+
+	volSize_.set(0);
+	numDisks_.set(1);
+	diskNum_.set(1);
+	sectorSize_.set(2048);
+	pathTableSize_.set(0);
+	pathTableLoc_ = 0;
+	altPTLoc_ = 0;
+	bigEndPTL_ = 0;
+	bigEndAPTL_ = 0;
+
+	//rootDE_
+
+	memset(volSetId_, ' ', 128);
+	memset(pubId_   , ' ', 128);
+	memset(prepId_  , ' ', 128);
+
+	memset(appId_   , ' ', 128);
+	static const char *const appId = "NedOS mkcd Utility";
+	memcpy(appId_, appId, strlen(appId)-1);
+
+	memset(copyFile_, ' ', 38);
+	memset(absFile_ , ' ', 36);
+	memset(bioFile_ , ' ', 37);
+
+	//volCreate_;
+	//volMod_;
+	//volExp_;
+	//volEff_;
+
+	fsVer_   = 1;
+	unused3_ = 0;
+
+	memset(appAvail_, 0, 512);
+	memset(rsvd_,     0, 653);
+}
+
+void PrimaryVolDesc::print(void)
+{
+	printf("Type: %d\n", type_);
+	printStr(systemId_, 32);
+	printStr(volumeId_, 32);
+	printf("VolSize : %d\n", volSize_.little_);
+	printf("NumDisks: %d\n", numDisks_.little_);
+	printf("DiskNum : %d\n", diskNum_.little_);
+	printf("SectorSz: %d\n", sectorSize_.little_);
+	printf("PathTblS: %d\n", pathTableSize_.little_);
+	printf("PathTblL: %d\n", pathTableLoc_);
+	printf("AltPTL  : %d\n", altPTLoc_);
+
+	rootDE_.print();
+
+	printStr(volSetId_, 128);
+	printStr(pubId_, 128);
+	printStr(prepId_, 128);
+	printStr(appId_, 128);
+
+	printStr(copyFile_, 38);
+	printStr(absFile_, 36);
+	printStr(bioFile_, 37);
+}
+
+//----------------------------------------------------------------------------
+NullDesc::NullDesc(void)
+{
+	type_ = 255;
+}
+
---
+++ mkcd/iso9660.h
@@ -0,0 +1,128 @@
+#ifndef ISO9660_H
+#define ISO9660_H
+
+#include <stdint.h>
+
+struct VolDescBase
+{
+	uint8_t type_;
+	uint8_t cd001_[5];
+	uint8_t version_; // 1
+
+	VolDescBase(void);
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+struct BiEnd32
+{
+	uint32_t little_;
+	uint32_t big_;
+
+	void set(uint32_t val);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+struct BiEnd16
+{
+	uint16_t little_;
+	uint16_t big_;
+
+	void set(uint16_t val);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+// 34 bytes
+struct DirEnt
+{
+	uint8_t weird_[34];
+
+public: // methods
+	DirEnt(void);
+
+	void print(void);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+// all fields in ascii, except TZ
+struct DateTime
+{
+	uint8_t  year_[4];
+	uint8_t month_[2];
+	uint8_t   day_[2];
+	uint8_t  hour_[2];
+	uint8_t   min_[2];
+	uint8_t   sec_[2];
+	uint8_t  hsec_[2];
+
+	uint8_t  tz_; // -48..52
+
+public: // methods
+	DateTime(void);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+struct PrimaryVolDesc : public VolDescBase
+{
+	//type_ = 1
+	uint8_t  unused0_;
+	uint8_t  systemId_[32];
+	uint8_t  volumeId_[32];
+	uint8_t  unused1_[8];
+	BiEnd32  volSize_; // in 2KB sectors
+	uint8_t  unused2_[32];
+	BiEnd16  numDisks_;
+	BiEnd16  diskNum_;
+	BiEnd16  sectorSize_;
+	BiEnd32  pathTableSize_; // in bytes
+	uint32_t pathTableLoc_; // LBA
+	uint32_t altPTLoc_; // LBA
+	uint32_t bigEndPTL_; // LBA
+	uint32_t bigEndAPTL_; // LBA
+
+	DirEnt   rootDE_;
+
+	uint8_t  volSetId_[128];
+	uint8_t     pubId_[128];
+	uint8_t    prepId_[128];
+	uint8_t     appId_[128];
+
+	uint8_t copyFile_[38];
+	uint8_t  absFile_[36]; // not a typo
+	uint8_t  bioFile_[37]; // not a typo
+
+	DateTime volCreate_;
+	DateTime volMod_;
+	DateTime volExp_;
+	DateTime volEff_;
+
+	uint8_t  fsVer_; // 1
+	uint8_t  unused3_;
+
+	uint8_t  appAvail_[512];
+	uint8_t  rsvd_[653];
+
+public: // methods
+	PrimaryVolDesc(void);
+
+	void print(void);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+struct NullDesc : public VolDescBase
+{
+	//type_ = 255
+	uint8_t unused_[2041];
+
+public:
+	NullDesc(void);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+#endif
+
