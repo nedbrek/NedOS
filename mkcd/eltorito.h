6195dd69f1a1e610f7ef1592d0a365e70aebbbee
Edward Brekelbaum <nedbrek@yahoo.com>
2010-01-26 13:01:05 +0000

    Added BootCatalog structures

--- mkcd/eltorito.h
+++ mkcd/eltorito.h
@@ -19,5 +19,44 @@ public: // methods
 } __attribute__((packed));
 // g++ requires two parens for some bizarre reason
 
+struct BCEntry
+{
+	uint8_t b_[32];
+
+	BCEntry(void);
+
+	void print(void);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+struct ValidationEntry // IsA BCEntry
+{
+	uint8_t  headerID_; // 1
+	uint8_t  platformID_; // 0 x86
+	uint16_t rsvd_; // 0
+	uint8_t  idStr_[24];
+	int16_t  checksum_;
+	uint8_t  key1_; // 0x55
+	uint8_t  key2_; // 0xaa
+
+	ValidationEntry(void);
+
+	bool isValid(void) const;
+
+	void print(void);
+
+} __attribute__((packed));
+// g++ requires two parens for some bizarre reason
+
+struct BootCatalog
+{
+	BCEntry entries_[64];
+
+	BootCatalog(void);
+
+	void print(void);
+};
+
 #endif
 
