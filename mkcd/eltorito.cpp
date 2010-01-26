d2d1dc8195fb2ae5e5444f51d4f4f2c9a84de825
Edward Brekelbaum <nedbrek@yahoo.com>
2010-01-26 13:01:37 +0000

    Impl for BootCatalog structures

--- mkcd/eltorito.cpp
+++ mkcd/eltorito.cpp
@@ -1,4 +1,5 @@
 #include "eltorito.h"
+#include "print.h"
 #include <cstring>
 #include <cstdio>
 
@@ -20,5 +21,52 @@ void BootRecord::print(void)
 {
 	printf("Type: %d\n", type_);
 	printf("Boot Catalog sector: %d\n", bootCatLoc_);
+}
+
+//----------------------------------------------------------------------------
+BCEntry::BCEntry(void)
+{
+}
+
+void BCEntry::print(void)
+{
+	printStr(b_, 32);
+}
+
+//----------------------------------------------------------------------------
+ValidationEntry::ValidationEntry(void)
+{
+}
+
+bool ValidationEntry::isValid(void) const
+{
+	return headerID_ == 1 && key1_ == 0x55 && key2_ == 0xaa;
+}
+
+void ValidationEntry::print(void)
+{
+	printf("ID=%d\n", headerID_);
+	printf("PlatformID=%d\n", platformID_);
+	printf("idStr=\n");
+	printStr(idStr_, 24);
+	printf("Checksum=%d\n", checksum_);
+}
+
+//----------------------------------------------------------------------------
+BootCatalog::BootCatalog(void)
+{
+}
+
+void BootCatalog::print(void)
+{
+	ValidationEntry *ve = (ValidationEntry*)entries_;
+
+	if( ve->isValid() )
+		ve->print();
+	else
+		printf("Bad validation entry\n");
+
+	for(unsigned i = 1; i < 64; ++i)
+		entries_[i].print();
 }
 
