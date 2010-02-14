67e5a46374ecd594a794fcbab9fa54be19ebbc5e
Edward Brekelbaum <nedbrek@yahoo.com>
2010-02-14 10:56:13 +0000

    Moved stack from top of seg to before boot code Fixed memory map to reflect recent changes Shuffled BOB to have VBE buffers at end of seg Added code to retrieve VBE info and decide on a mode (saved in BOB)

--- mkcd/boot.asm
+++ mkcd/boot.asm
@@ -2,13 +2,14 @@
 ; memory map
 ; 00000..003FF	IDT
 ; 00400..004FF BIOS data
-; 00500..07BFF free
+; 00500..07BFF stack (30464 bytes)
 ; 07C00..07DFF boot sector
 ; 07E00..0FFFF free
-; 10000..1FFFF GDT+IDT
-; 20000..2FFFF stage 1 page tables
-; 30000..3FFFF memory map
-; 40000..7FFFF free
+; 10000..1FFFF boot output block
+; 20000..2FFFF GDT+IDT
+; 30000..3FFFF stage 1 page tables
+; 40000..4FFFF memory map
+; 50000..7FFFF free
 ; 80000..9FBFF possible BIOS EDA
 ; 9FC00..9FFFF definite BIOS EDA
 ; A0000..FFFFF ROM
@@ -30,7 +31,7 @@ BIOS_SET_A20M	equ	0x2401
 	; set up the stack (needed for BIOS calls)
 	xor eax, eax
 	mov ss, ax
-	mov sp, 0xffff
+	mov sp, 0x7c00
 
 	xor   di,  di
 
@@ -40,21 +41,116 @@ BIOS_SET_A20M	equ	0x2401
 	mov  cx, 0x8000
 	rep  stosw
 
-	mov [ES:1024], dx
+	xor ebx, ebx
+	mov [es:bx], dx
 
-	; get mem map
-	push (MMAP_BASE >> 4)
-	pop  es
-	mov  cx, 0x8000
-	rep  stosw
+	; set VGA mode
+
+	;; first get VESA info
+	mov di, 0xfd00
+	mov DWORD [es:di], '2EBV' ; VBE2 little endian
+	mov ax, 0x4f00
+	int 0x10
+	cmp ax, 0x004f
+	jne error
+
+	;; search the mode blocks for one we like
+	lds si, [es:di+14] ; load list seg:off into ds:si
+	add di, 0x200 ; shift past VESA info block
 
 	xchg bx,bx
 
-	xor ebx, ebx ; ask for start of map
+	;; fill the mode info block
+top_vbe:
+	mov cx, [si] ; mode we will consider
+	cmp cx, 0xffff ; -1 is end of list
+	je  done_vbe
+
+	mov ax, 0x4f01
+	int 0x10
+	cmp ax, 0x004f
+	jne error
+
+	;; space saver, ds=es (needs to be restored)
+	push ds
+	push es
+	pop  ds
+
+	;; check the attributes for hw sup, color, graphics, lfb
+	mov ax, [di]
+	and ax, 0x99
+	cmp ax, 0x99
+	jne next_vbe
+
+	;; examine x,y,bpp,mm to see if we want it
+	mov ax, [di+27] ; get mem mode
+	and al, 0xfd ; check 4 and 6 (non-banked)
+	cmp al, 4
+	jne next_vbe
+
+	movzx ax, [di+25] ; get bpp
+	cmp   BYTE [12], 32
+	jne   vbe_not_32bpp
+
+	;;; if we already have 32 bpp
+	;;; only want higher x in 32 bpp
+	cmp ax, 32
+	jne next_vbe
+
+vbe_check_width:
+	;;; check width
+	mov ax, [di+18] ; get width
+	;;; don't want more than 1024
+	cmp ax, 1024
+	ja  next_vbe
+
+	;;; compare widths
+	cmp ax, [4]
+	jbe next_vbe ; less or same, skip
+	jmp save_vbe ; more, save
+
+vbe_not_32bpp:
+	;;; current mode is not 32bpp (ax is new bpp)
+	cmp ax, [12]
+	jb  next_vbe        ; less, skip
+	ja  save_vbe        ; more, take it
+	jmp vbe_check_width ; equal, check width
+
+save_vbe:
+	mov   ax, [di+18] ; get width
+	mov   [4], ax
+	mov   ax, [di+20] ; get height
+	mov   [8], ax
+	movzx ax, [di+25] ; get bpp
+	mov   [12], ax
+	mov   ax, [di+40] ; get lfb
+	mov   [16], ax
+	mov   ax, [di+42] ; get lfb
+	mov   [18], ax
+	movzx ax, [di] ; get caps
+	mov   [20], ax
+	mov   [24], cx
+
+next_vbe:
+	pop ds
+	add si, 2
+	jmp top_vbe
+
+done_vbe:
+
+	; get mem map (assumes ebx is 0)
+	xor  di, di
+	push (MMAP_BASE >> 4)
+	pop  es
+	xor  ax, ax
+	mov  cx, 0x8000
+	rep  stosw
+
 	mov ecx, 24  ; ACPI 3
 
-	; first call is CF==error
+	;; first call is CF==error
 	mov  ax, BIOS_GET_MMAP
+	;xor ebx, ebx
 	mov edx, 'PAMS' ; 'SMAP' little endian
 	mov BYTE [es:di+20], 1 ; set ACPI 3 valid
 
@@ -107,11 +203,10 @@ incMap:
 	jmp nextMap
 
 error:
+	xchg bx,bx
 	jmp error
 
 doneMap:
-
-	; set VGA mode
 
 	; set A20
 	mov  ax, BIOS_SET_A20M
