159dc08eb0a6018d2e33426cb4ec2775627d5465
Edward Brekelbaum <nedbrek@yahoo.com>
2010-04-03 18:02:23 +0000

    Moved font bmps from kernel.asm to font.asm

--- mkcd/kernel.asm
+++ mkcd/kernel.asm
@@ -361,44 +361,5 @@ hi_str:
 cap_h: ; 72
 	dq 0x228A2FA28A2000
 
-low_e: ; 101
-	dq 0x1c8be81c000
-low_f: ; 102
-	dq 0xc490f10410000
-low_g: ; 103
-	dq 0x1e8a278289c
-low_h: ; 104
-	dq 0x2082cca28a2000
-low_i: ; 105
-	dq 0x801820821c000
-low_j: ; 106
-	dq 0x200608209248c
-low_k: ; 107
-	dq 0x20822938922000
-low_l: ; 108
-	dq 0x1820820821c000
-low_m: ; 109
-	dq 0x34aaaaa2000
-low_n: ; 110
-	dq 0x2cca28a2000
-low_o: ; 111
-	dq 0x1c8a289c000
-low_p: ; 112
-	dq 0x2cca2cac820
-low_q: ; 113
-	dq 0x1a9a299a082
-low_r: ; 114
-	dq 0x2cca0820000
-low_s: ; 115
-	dq 0x1c81c0bc000
-low_t: ; 116
-	dq 0x1043c41048c000
-low_u: ; 117
-	dq 0x228a299a000
-low_v: ; 118
-	dq 0x22894508000
-low_w: ; 119
-	dq 0x228aaa94000
-low_x: ; 120
-	dq 0x22508522000
+%include "font.asm"
 
