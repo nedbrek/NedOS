688b4640a919099964832dab1cd4dab6f0b1795c
Edward Brekelbaum <nedbrek@yahoo.com>
2011-11-13 08:02:25 -0500

    MMAP - moved input buffer to end of BOB Clarified comments

--- mkcd/bob.asm
+++ mkcd/bob.asm
@@ -6,8 +6,8 @@ VGA_LFBP    equ 16
 VGA_CAPS    equ 20
 VGA_MODE    equ 24
 
-; 8K located at the top of the 64K region
-INPUT_QUEUE equ 0x1_0000 - 0x2000
+; 8K located at the top of the 64K BOB region
+INPUT_QUEUE equ BOOT_PARMS + 0x1_0000 - 0x2000
 ; start and end pointers located in different cache lines
 QUEUE_START equ INPUT_QUEUE - 32
 QUEUE_END   equ INPUT_QUEUE - 64
--- mkcd/mmap.asm
+++ mkcd/mmap.asm
@@ -1,16 +1,16 @@
 ; memory map
-; 00000..003FF IDT
-; 00400..004FF BIOS data
-; 00500..07BFF stack (30464 bytes)
-; 07C00..07DFF boot sector
-; 07E00..3FFFF free
-; 40000..4FFFF boot output block
-; 50000..5FFFF GDT+IDT
-; 60000..6FFFF stage 1 page tables
-; 70000..7FFFF memory map
-; 80000..9FBFF possible BIOS EDA
-; 9FC00..9FFFF definite BIOS EDA
-; A0000..FFFFF ROM
+; 0_0000..0_03FF Real-mode IDT (reclaimable)
+; 0_0400..0_04FF BIOS data (reclaimable)
+; 0_0500..0_7BFF boot stack (30464 bytes)
+; 0_7C00..0_7DFF boot sector (reclaimable)
+; 0_7E00..3_FFFF free
+; 4_0000..4_FFFF boot output block
+; 5_0000..5_FFFF GDT+IDT
+; 6_0000..6_FFFF stage 1 page tables
+; 7_0000..7_FFFF memory map
+; 8_0000..9_FBFF possible BIOS EDA
+; 9_FC00..9_FFFF definite BIOS EDA
+; A_0000..F_FFFF ROM
 
 BOOT_PARMS     equ   0x40000 ; information from boot time
 TSS_BASE       equ   0x50000
