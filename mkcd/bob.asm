cc450f45b518767d5d11a00b7b655e45b4288d30
Edward Brekelbaum <nedbrek@yahoo.com>
2012-09-29 09:51:25 -0400

    bob.asm    Added structure definitions: StrVtbl, CwrappedStringNear, BasicMap, IntString    Added return codes

--- mkcd/bob.asm
+++ mkcd/bob.asm
@@ -21,6 +21,45 @@ struc BasicString
 	.vec:  resb Vector_size
 endstruc
 
+struc StrVtbl
+	.typeInfo: resd 1
+	.delete  : resd 1
+	.clone   : resd 1
+	.incRef  : resd 1
+	.decRef  : resd 1
+
+	.clear   : resd 1
+	.length  : resd 1
+	.append_near: resd 1
+	.append_far : resd 1
+
+	.compare: resd 1
+	.intVal: resd 1
+	.lookup: resd 1
+
+	.run: resd 1
+endstruc
+
+struc CwrappedStringNear
+	.vtbl: resd 1
+	.ref:  resd 1
+	.len:  resd 1
+	.act:  resd 1
+endstruc
+
+struc BasicMap
+	.vtbl:    resd 1
+	.ref:     resd 1
+	.vecTags: resb Vector_size
+	.vecLocs: resb Vector_size
+endstruc
+
+struc IntString
+	.vtbl: resd 1
+	.ref:  resd 1
+	.val:  resq 1
+endstruc
+
 struc TermContext
 	.consoleX: resd 1; console x pos (px)
 	.consoleY: resd 1; console y pos (px)
@@ -37,3 +76,7 @@ INPUT_QUEUE equ BOOT_PARMS + 0x1_0000 - INPUT_QUEUE_SIZE
 QUEUE_START equ INPUT_QUEUE - 32
 QUEUE_END   equ INPUT_QUEUE - 64
 
+RET_OK    equ 0
+RET_ERR   equ 1
+RET_BREAK equ 2
+RET_CONT  equ 3
